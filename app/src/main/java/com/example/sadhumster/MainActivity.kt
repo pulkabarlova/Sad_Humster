a280c1fc01a15c854107cef162ae38c4fb2c6270
polina <barlovapolina@gmail.com>
2024-09-23 20:05:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sadhumster/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.sadhumster
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.media.MediaPlayer
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun on_click_media(view: View) {
+        MediaPlayer.create(this, R.raw.sad_humster_song).start()
+    }
+}
