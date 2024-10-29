75ff62b6a7ef2a8eb26798060c595d7701c5c5f0
polina <barlovapolina@gmail.com>
2024-10-29 20:47:57 +0100

    Recycle с загадками

---
+++ app/src/main/java/com/example/sadhumster/Joke.kt
@@ -0,0 +1,7 @@
+package com.example.sadhumster
+
+data class Joke(
+    val category: String,
+    val question: String,
+    val answer: String
+)
---
+++ app/src/main/java/com/example/sadhumster/JokesDiffUtil.kt
@@ -0,0 +1,25 @@
+package com.example.sadhumster
+
+import androidx.recyclerview.widget.DiffUtil
+
+class JokesDiffUtil (
+    private val oldList: List<Joke>,
+    private val newList: List<Joke>) : DiffUtil.Callback()
+    {
+        override fun getOldListSize(): Int {
+            return oldList.size
+        }
+
+        override fun getNewListSize(): Int {
+            return newList.size
+        }
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].category == newList[newItemPosition].category &&
+                    oldList[oldItemPosition].question == newList[newItemPosition].question
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == newList[newItemPosition]
+        }
+    }
